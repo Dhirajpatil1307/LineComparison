4b6f86b3a9084af164eb72def4c7c957c94f1714
dhirajp504@gmail.com <dhirajp504@gmail.com>
2021-07-04 14:19:51 +0530

    Added: Displaying Welcome Message

---
+++ src/line_comparison/WelcomeToLineComparisonComparison.java
@@ -0,0 +1,8 @@
+package line_comparison;
+
+public class WelcomeToLineComparisonComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Problem");
+	}
+}
